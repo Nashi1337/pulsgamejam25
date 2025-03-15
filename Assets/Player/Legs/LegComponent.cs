a46cd94494a1a37587cbf9c28c74b8deca235bf3
simon <you@example.com>
2025-03-15 17:49:12 +0100

    Created LegStats so that common Vars can quickly be changed

--- Assets/Player/Legs/LegComponent.cs
+++ Assets/Player/Legs/LegComponent.cs
@@ -5,8 +5,35 @@ namespace Player.Legs
 {
     public interface LegComponent
     {
+        const double NotMovingDelta = 10e-3;
+        LegStats Stats { get; set; }
         
-        public void Move(Vector2 direction, Rigidbody2D rb);
-        public void Jump( Rigidbody2D rb, bool isGrounded);
+        public void Move(Vector2 direction, Rigidbody2D rb)
+        {
+            float targetVelocity = direction.x * Stats.maxSpeed;
+
+            if (direction.x == 0)
+            {
+                rb.linearVelocityX = Mathf.Lerp(rb.linearVelocityX, targetVelocity, Time.deltaTime * Stats.groundDampening);
+
+                if (Mathf.Abs(rb.linearVelocityX) < NotMovingDelta)
+                {
+                    rb.linearVelocityX = 0;
+                }
+            }
+            else
+            {
+                rb.linearVelocityX = targetVelocity;
+            }
+
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, rb.linearVelocity.y);
+        }
+
+        public void Jump(Rigidbody2D rb, bool isGrounded)
+        {
+            if(isGrounded)
+                rb.AddForce(new Vector2(0, Stats.jumpHeight), ForceMode2D.Impulse);
+        }
+        
     }
 }
